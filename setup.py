9c3f77117c848de8bb96117d581701a6b6277482
Pavol Rusnak <stick@gk2.sk>
2015-05-26 00:17:28 +0200

    update setup.py to new fork

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ from setuptools import setup
 
 setup(
     name = 'bip32utils',
-    version = '0.2',
+    version = '0.3',
     author = 'Johnathan Corgan, Corgan Labs',
     author_email = 'johnathan@corganlabs.com',
-    url = 'http://github.com/jmcorgan/bip32utils',
+    maintainer = 'Pavol Rusnak, SatoshiLabs',
+    maintainer_email = 'stick@satoshilabs.com',
+    url = 'http://github.com/satoshilabs/bip32utils',
     description = 'Utilities for generating and using Bitcoin Hierarchical Deterministic wallets (BIP0032).',
     license = 'MIT',
     requires = ['ecdsa'],
